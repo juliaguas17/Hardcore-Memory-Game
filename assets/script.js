bbda50475e91582efcc0e323b9427c75fb94b365
jengle-dev <j.engle.dev@gmail.com>
2023-01-25 20:14:16 -0600

    saving to see if same CORS error occurs on others' machines

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
+//API key	uC6CsrKOpLvqkHaklwGh6uV8LwfEqa5t4GsVnRYFpbGD5a58OzhIGQGQk86DMX1C
+//Client ID	U7WHdyA6jueqIJWOJH775Wqqn4ZlhybDLxZHvoTx9NJoMyQLNdbwXWy4vKi5IDqn
+
+var APIkey = 'uC6CsrKOpLvqkHaklwGh6uV8LwfEqa5t4GsVnRYFpbGD5a58OzhIGQGQk86DMX1C';
+var fullAPIkey = 'Bearer ' + APIkey; //is working
+
+console.log(fullAPIkey);
 // Declare global variables
+
+//Need to decide how many different animals. 3-5 
+
+function fetchIcon() {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: fullAPIkey, //is working
+        }
+    };
+    console.log(options); //is working
+
+    fetch('https://api.iconfinder.com/v4/icons/search?query=arrow&count=10', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
+fetchIcon();
