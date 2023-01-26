6063069bcb832d6a16bfb14bbca30f1a781dec51
jengle-dev <j.engle.dev@gmail.com>
2023-01-25 22:25:16 -0600

    got API key and copied code from serpapi.com to get google search results

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+//jen's API a4a6db4f2015502f33fd07a0dabf3c5c4f2804cceedd61c91db6f93d0cff030a
+
+//hardcode link for animals query "https://www.google.com/search?q=animals&oq=animals&uule=w+CAIQICIaQXVzdGluLFRleGFzLFVuaXRlZCBTdGF0ZXM&gl=us&tbm=isch&sourceid=chrome&ie=UTF-8"
 
 var apiUrl = "https://serpapi.com/search?q=";
 var theme = 'animals'
@@ -6,13 +9,19 @@ console.log(searchUrl);
 
 import { getJson } from "serpapi";
 
+async function searchAPI(){
 const params = {
+  api_key: "a4a6db4f2015502f33fd07a0dabf3c5c4f2804cceedd61c91db6f93d0cff030a",
   q: "animals",
-  tbm: "isch",
-  ijn: "0",
-  api_key: "secret_api_key"
+  location: "Austin, Texas, United States",
+  google_domain: "google.com",
+  gl: "us",
+  hl: "en",
+  tbm: "isch"
 };
 
 // Show result as JSON
 const response = await getJson("google", params);
-console.log(response["images_results"]);
+console.log(response);
+}
+searchAPI();
