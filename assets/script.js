97f425356bc4c6fa0e82e6132880776168d539d5
jengle-dev <j.engle.dev@gmail.com>
2023-01-26 22:01:01 -0600

    updated script js to include the flickr api and http.jpg links for the flipped cards

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,42 @@
-//jen's API a4a6db4f2015502f33fd07a0dabf3c5c4f2804cceedd61c91db6f93d0cff030a
+// card options
+const cardArray = [];
 
-//hardcode link for animals query "https://www.google.com/search?q=animals&oq=animals&uule=w+CAIQICIaQXVzdGluLFRleGFzLFVuaXRlZCBTdGF0ZXM&gl=us&tbm=isch&sourceid=chrome&ie=UTF-8"
 
-var apiUrl = "https://serpapi.com/search?q=";
-var theme = 'animals'
-var searchUrl = apiUrl + theme;
-console.log(searchUrl);
+//https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+// https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
+// { "id": "52650717452", "owner": "30706946@N00", "secret": "f3aac9950a", "server": "65535", "farm": 66, "title": "Another Idiom", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+// https://live.staticflickr.com/65535/52650717452_f3aac9950a_s.jpg
 
-import { getJson } from "serpapi";
+var apiKey = 'ab44015fa7a49dd092d72824c1d221fd';
+// var flickrServerId = //pull out of json;
 
-async function searchAPI(){
-const params = {
-  api_key: "a4a6db4f2015502f33fd07a0dabf3c5c4f2804cceedd61c91db6f93d0cff030a",
-  q: "animals",
-  location: "Austin, Texas, United States",
-  google_domain: "google.com",
-  gl: "us",
-  hl: "en",
-  tbm: "isch"
-};
+function returnData() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ab44015fa7a49dd092d72824c1d221fd&tags=animals&per_page=8&format=json&nojsoncallback=1',
 
-// Show result as JSON
-const response = await getJson("google", params);
-console.log(response);
+        success: function (result) {
+            console.log(result);
+            //generate array of photo urls
+            let photoURLs = result.photos.photo.map(createPhotoUrl);
+            console.log(photoURLs);
+            //pass that array into a function
+            //map can take each element of it and map to something array
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    })
 }
-searchAPI();
+returnData();
+
+/**
+ * this function takes an object and returns a photo url
+ * @param {*} obj 
+ * @returns 
+ */
+function createPhotoUrl(obj) {
+    return 'https://live.staticflickr.com/' + obj.server + "/" + obj.id + "_" + obj.secret + "_s.jpg";
+
+    // return 'The '+ obj.Common-Name + ' lives in a'
+} 
