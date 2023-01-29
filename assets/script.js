a0e0200e97c901693ae4fb1ca6846958ede835c7
jengle-dev <j.engle.dev@gmail.com>
2023-01-29 17:04:24 -0600

    updated attempt to get cards in code, not ready to merge

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,7 @@
-// https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
-// https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
-// { "id": "52650717452", "owner": "30706946@N00", "secret": "f3aac9950a", "server": "65535", "farm": 66, "title": "Another Idiom", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
-// https://live.staticflickr.com/65535/52650717452_f3aac9950a_s.jpg
 
 var apiKey = 'ab44015fa7a49dd092d72824c1d221fd';
+
 // var flickrServerId = //pull out of json;
-
-// document.addEventListener('click', searchBtn)
-//     window.location = 'game.html';
-// card options
-const cardArray = [
-    {
-        name: 'Pic1',
-        img: photoURLs[0],
-    },
-    {
-        name: 'Pic1',
-        img: photoURLs[0],
-    },
-    {
-        name: 'Pic2',
-        img: photoURLs[1],
-    },
-    {
-        name: 'Pic2',
-        img: photoURLs[1],
-    },
-    {
-        name: 'Pic3',
-        img: photoURLs[2],
-    },
-    {
-        name: 'Pic3',
-        img: photoURLs[2],
-    },
-    {
-        name: 'Pic4',
-        img: photoURLs[3],
-    },
-    {
-        name: 'Pic4',
-        img: photoURLs[3],
-    },
-    {
-        name: 'Pic5',
-        img: photoURLs[4],    
-    },
-    {
-        name: 'Pic5',
-        img: photoURLs[4],   
-    },
-    {
-        name: 'Pic6',
-        img: photoURLs[5],   
-    },
-    {
-        name: 'Pic6',
-        img: photoURLs[5],    
-    },
-    {
-        name: 'Pic7',
-        img: photoURLs[6],    
-    },
-    {
-        name: 'Pic7',
-        img: photoURLs[6],    
-    },
-    {
-        name: 'Pic8',
-        img: photoURLs[7],    
-    },
-    {
-        name: 'Pic8',
-        img: photoURLs[7],    
-    },
-]
-
 function returnData() {
     $.ajax({
         method: 'GET',
@@ -86,34 +12,55 @@ function returnData() {
             //generate array of photo urls
             let photoURLs = result.photos.photo.map(createPhotoUrl);
             console.log(photoURLs);
-            //pass that array into a function
             //map can take each element of it and map to something array
+            // photoURLs.forEach('img', i)
+            // document.getElementsByClassName(card).appendChild(img);
+            for (var i = 0; i < photoURLs.length; i++) {
+                var cardImg = document.createElement('img');
+                document.getElementsByID(card).appendChild(img);
+                cardImg.setAttribute('src', 'assets/white-empty-square.png');
+                console.log(photoURLs.length);
+                // card.setAttribute('a', 'img');
+                cardImg.setAttribute('data-id', i);
+                //listen for click and invoke a flipcard function
+                cardImg.addEventListener('click', flipCard);
+                grid.appendChild(cardImg);
+            }
         },
+
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
-    })
-}
-returnData();
+    });
 
-/**
- * this function takes an object and returns a photo url
- * @param {*} obj 
- * @returns 
- */
-function createPhotoUrl(obj) {
-    return 'https://live.staticflickr.com/' + obj.server + "/" + obj.id + "_" + obj.secret + "_s.jpg";
-
-    // return 'The '+ obj.Common-Name + ' lives in a'
-} 
-
-function buildImages() {
-    for (var i = 0; i < cardArray.length; i++) {
-        var card = document.createElement('img');
-            card.setAttribute('src', cardArray.img[i]);
-            card.setAttribute('data-id', i);
-            //listen for click and invoke a flipcard function
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
+    /**
+     * this function takes an object and returns a photo url
+     * @param {*} obj 
+     * @returns 
+     */
+    function createPhotoUrl(obj) {
+        return 'https://live.staticflickr.com/' + obj.server + "/" + obj.id + "_" + obj.secret + "_s.jpg"; //this is working
+        // return 'The '+ obj.Common-Name + ' lives in a'
     }
 }
+returnData(); //this and its function are working
+
+//photoURLs is an array built by the createPhotoUrl & map method;
+//
+// function buildImages() {
+//     for (var i = 0; i < photoURLs.length; i++) {
+//         var card = document.createElement('img');
+//         document.getElementsByClassName(card).appendChild(img);
+//         console.log(photoURLs.length);
+//         // card.setAttribute('a', 'img');
+//         card.setAttribute('data-id', i);
+//         //listen for click and invoke a flipcard function
+//         card.addEventListener('click', flipCard);
+//         grid.appendChild(card);
+//     }
+// }
+// buildImages();
+
+
+
+// for each item in array, creat an a href inside of div tag-with card class and attribute(s)?
