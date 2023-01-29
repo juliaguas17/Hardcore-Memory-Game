4d5d197705736f9d3ff651b51b6f5929db1a625a
Julianne <juliaguas17@gmail.com>
2023-01-28 20:33:44 -0600

    fixed timer function and added modal alert pseudocode

--- assets/script-timerTest.js
+++ assets/script-timerTest.js
@@ -1,27 +1,21 @@
-event.preventDefault
+// Timer function
 
-var countDownDate = localStorage.getItem('startDate');
-if (countDownDate) {
-    countDownDate = new Date(countDownDate);
-} else {
-    countDownDate = new Date();
-    localStorage.setItem('startDate', countDownDate);
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 }
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var distance = now - countDownDate.getTime();
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("count-up-timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";}, 1000);
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+};
