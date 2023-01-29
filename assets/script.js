a77560a6c0d4a42351f1b72d89e4f2cc74c317c2
jengle-dev <j.engle.dev@gmail.com>
2023-01-28 19:57:34 -0600

    updates to js and some html and some css and moved timerTest to assets

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,92 @@
-// card options
-const cardArray = [];
-
-
-//https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+// https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
 // https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 // { "id": "52650717452", "owner": "30706946@N00", "secret": "f3aac9950a", "server": "65535", "farm": 66, "title": "Another Idiom", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
 // https://live.staticflickr.com/65535/52650717452_f3aac9950a_s.jpg
 
 var apiKey = 'ab44015fa7a49dd092d72824c1d221fd';
 // var flickrServerId = //pull out of json;
+
+// document.addEventListener('click', searchBtn)
+//     window.location = 'game.html';
+// card options
+const cardArray = [
+    {
+        name: 'Pic1',
+        img: photoURLs[0]
+    },
+    {
+        name: 'Pic1',
+        img: photoURLs[0]
+    },
+    {
+        name: 'Pic2',
+        img: photoURLs[1]
+    },
+    {
+        name: 'Pic2',
+        img: photoURLs[1]
+    },
+    {
+        name: 'Pic3',
+        img: photoURLs[2]
+    },
+    {
+        name: 'Pic3',
+        img: photoURLs[2]
+    },
+    {
+        name: 'Pic4',
+        img: 'assets/images/purple.png'
+    },
+    {
+        name: 'Pic4',
+        img: 'assets/images/purple.png'
+    },
+    {
+        name: 'Pic5',
+        img: 'assets/images/yellow.png'
+    },
+    {
+        name: 'Pic5',
+        img: 'assets/images/yellow.png'
+    },
+    {
+        name: 'Pic6',
+        img: 'assets/images/Orange.png'
+    },
+    {
+        name: 'Pic6',
+        img: 'assets/images/Orange.png'
+    },
+    {
+        name: 'Pic7',
+        img: 'assets/images/Orange.png'
+    },
+    {
+        name: 'Pic7',
+        img: 'assets/images/Orange.png'
+    },
+    {
+        name: 'Pic8',
+        img: 'assets/images/Orange.png'
+    },
+    {
+        name: 'Pic8',
+        img: 'assets/images/Orange.png'
+    },
+]
+
+function buildImages() {
+    for (var i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img');
+            card.setAttribute('src', cardArray.img[i]);
+            card.setAttribute('data-id', i);
+            //listen for click and invoke a flipcard function
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+    }
+}
+
 
 function returnData() {
     $.ajax({
