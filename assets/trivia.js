67de57a6dc5cdbf887aae2e8907eecec907c51d8
Julianne <juliaguas17@gmail.com>
2023-01-30 20:04:37 -0600

    edited modal alert in HTML

--- assets/trivia.js
+++ assets/trivia.js
@@ -1,7 +1,7 @@
 // https://api-ninjas.com/api/animals
 // jen api key btEPU29q9pKigtx0a4g47g==bJB66T4QMs6eLCx1
 
-var animalArray = ['cheetah', 'frog', 'owl'];
+var animalNameArray = [];
 var APIkey = 'btEPU29q9pKigtx0a4g47g==bJB66T4QMs6eLCx1';
 var animalName = 'cheetah';
 
@@ -14,6 +14,9 @@ $.ajax({
     
     success: function(result) {
         console.log(result);
+        //var animalNameArray = result.animalName.map(createAnimalNameArray);
+        //console.log(animalNameArray);
+        //console.log(animalName + " lives in " + animalLocation);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
@@ -22,3 +25,15 @@ $.ajax({
 })
 };
 returnTrivia();
+
+// When all of the cards are matched,
+// show initials form and button to display modal alert with trivia fact
+function gameOver () {
+    document.getElementById("initials-form").style.display=block;
+    document.getElementById("trivia-button").style.display=block;
+}
+
+// Displays trivia modal
+function showTrivia() {
+    document.getElementById("trivia-modal").style.display=block;
+}
